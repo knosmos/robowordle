84c1cefe72fed4866913027018369a7c943b586a
unknown <knosmos@gmail.com>
2022-02-27 16:40:49 -0500

    Initial commit

---
+++ april.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+from rich import print
+
+image = cv2.imread('printed.png')
+
+SIZE = 600
+
+# Get ROI corners
+
+arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_APRILTAG_36h11)
+arucoParams = cv2.aruco.DetectorParameters_create()
+(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+
+print(corners, ids, rejected)
+assert len(corners) == len(ids) == 4
+
+detected = [[ids[i], corners[i]] for i in range(4)]
+detected.sort(key = lambda x: x[0])
+print(detected)
+bounding_box = [
+    detected[0][1][0][2],
+    detected[1][1][0][3],
+    detected[2][1][0][0],
+    detected[3][1][0][1]
+]
+print(bounding_box)
+
+img_boxed = image.copy()
+cv2.polylines(img_boxed, np.int32([bounding_box]), True, (0, 255, 0), 2)
+# cv2.imshow('Fiducial Detection', img_boxed)
+
+# Dewarp
+vertices = [
+    [0, 0],
+    [SIZE, 0],
+    [SIZE, SIZE],
+    [0, SIZE]
+]
+matrix = cv2.getPerspectiveTransform(np.float32(bounding_box), np.float32(vertices))
+dewarped = cv2.warpPerspective(image, matrix, (SIZE, SIZE))
+cv2.imwrite('dewarped.png', dewarped)
+cv2.imshow('Dewarped', dewarped)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ color_detection.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+# Grid
+corners = [(227, 167), (373, 343)]
+grid_size = (5, 6)
+cell_size = [
+    (corners[1][0]-corners[0][0]) // grid_size[0],
+    (corners[1][1]-corners[0][1]) // grid_size[1]
+]
+
+# Colors
+GREY = np.array([98, 100, 127])
+WHITE = np.array([255, 255, 255])
+GREEN = np.array([72, 125, 105])
+YELLOW = np.array([67, 130, 162])
+
+colors = [["" for _ in range(grid_size[0])] for _ in range(grid_size[1])]
+
+img = cv2.imread('dewarped.png')
+for y in range(grid_size[1]):
+    for x in range(grid_size[0]):
+        mx = corners[0][0] + x * cell_size[0]
+        my = corners[0][1] + y * cell_size[1]
+
+        cv2.rectangle(img, (mx, my), (mx+cell_size[0], my+cell_size[1]), color=(0, 0, 255), thickness=2)
+        mean_color = np.array(cv2.mean(img[my:my+cell_size[0], mx:mx+cell_size[0]])[:-1])
+        
+        #cv2.imshow("cropped", img[my:my+cell_size[0], mx:mx+cell_size[0]])
+        #print(mean_color)
+        #cv2.waitKey()
+        
+        diffs = [
+            np.linalg.norm(mean_color - GREY),
+            np.linalg.norm(mean_color - WHITE),
+            np.linalg.norm(mean_color - GREEN),
+            np.linalg.norm(mean_color - YELLOW)
+        ]
+        min_diff = min(diffs)
+        min_index = diffs.index(min_diff)
+        best_color = ["W"," ","C","M"][min_index]
+
+        colors[y][x] = best_color
+print(colors)
+
+cv2.imshow('color grid', img)
+cv2.waitKey()
